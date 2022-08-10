d1336b5aedfb694eac28785d20011e0b8b397458
dragon-fire-fly <ek93@hotmail.co.uk>
2022-08-10 10:09:51 +0200

    Complete tasks 1-3

--- exercise.py
+++ exercise.py
@@ -2,3 +2,40 @@
 # upload to github for portfolio
 # thank me later when your git graph is as green as the python money u gonna earn
 
+#---- Task 1 - Basic math operators ----#
+
+# for n in range(0, 3):
+#     user_number = int(input("Please choose a number: "))
+#     print(user_number)
+#     if user_number % 2 == 0:
+#         print("Your number is even")
+#     else:
+#         print("Your number is odd")
+
+#---- Task 2 ----#
+
+# start_num = end_num = step = 1
+
+# user_range = int(input("Enter number of arguments to use (between 1 and 3): "))
+# if user_range > 0:
+#     start_num = int(input("Choose your starting number: "))
+# if user_range > 1:
+#     end_num = int(input("Choose your stopping number: "))
+# if user_range > 2:
+#     step = int(input("Choose your step number: "))
+
+# for n in range(start_num, end_num+1, step):
+#     print(n)
+
+
+#---- Task 3 ----#
+
+user_num = int(input("Please choose a number: "))
+
+for n in range (1, user_num):
+    if user_num % n == 0:
+        print(n)
+    
+
+
+
